c8afd68c53f16b1016e89b0169720754ad10a4ec
iamnaveen-2018 <naveenrajvikraman@gmail.com>
2025-02-08 20:23:02 +0530

    FirstCommit

---
+++ ExpenseManager/Models/Salary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpenseManager.Models;
+
+public partial class Salary
+{
+    public int SalaryId { get; set; }
+
+    public string SalarySource { get; set; } = null!;
+
+    public string SalaryType { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public decimal SalaryExpense { get; set; }
+
+    public decimal SalarySavings { get; set; }
+
+    public virtual ICollection<ExpenseCreation> ExpenseCreations { get; set; } = new List<ExpenseCreation>();
+}
